aec04e26cb06f0121c4d507d9a3d5a577ef0c6f0
Greg Westneat <gwestneat@neat-designs.com>
2019-09-02 21:05:44 -0700

    cool

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 import resolve from "rollup-plugin-node-resolve";
 import json from "rollup-plugin-json";
+import url from "rollup-plugin-url";
+import svgr from "@svgr/rollup";
 
 import pkg from "./package.json";
 
@@ -16,8 +19,13 @@ export default {
     }
   ],
   plugins: [
-    json(),
     external(),
+    json(),
+    postcss({
+      modules: true
+    }),
+    url(),
+    svgr(),
     babel({
       exclude: "node_modules/**",
       plugins: ["external-helpers"]
